0adc037bc4393f31518d960f4060891b79366ae2
Ayam Dobhal <dobhalayam@gmail.com>
2022-12-11 23:57:36 +0530

    cat

--- fcfs.c
+++ fcfs.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
-
-void findWaitingTime(int *, int, int *, int *);
-void findTurnAroundTime(int *, int, int *, int *, int *);
-void findAverageTime(int *, int, int *);
+void sort(int a[][2], int n);
 int main() {
     printf("Ayam Dobhal | Section C | University Roll Number 2016685\n");
     int n;
-    printf("Enter the number of processes: ");
+    printf("Enter the no. of process: ");
     scanf("%d", &n);
-    int processes[n];
-    printf("Enter the Process ID: ");
-    for (int i = 0; i < n; i++)
-        scanf("%d", &processes[i]);
-    printf("Enter the Burst Time: ");
-    int burst_time[n];
-    for (int i = 0; i < n; i++)
-        scanf("%d", &burst_time[i]);
-    findAverageTime(processes, n, burst_time);
+    int arr[n][2];
+    for (int i = 0; i < n; i++) {
+        printf("Enter the arrival and burst time for the %dth process: ", i + 1);
+        scanf("%d%d", &arr[i][0], &arr[i][1]);
+    }
+    sort(arr, n);
+    int wait[n], tat[n], averageWait = 0, averageTAT = 0;
+    wait[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wait[i] = wait[i - 1] + arr[i - 1][1];
+        averageWait += wait[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = wait[i] + arr[i][1];
+        averageTAT += tat[i];
+    }
+    double a = averageTAT / (1.0 * n), b = averageWait / (1.0 * n);
+    printf("Process no.\t Arrival time \t Burst Time\t Waiting Time \t Turn around Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t %d\t\t %d \t\t %d \t\t%d\n", i + 1, arr[i][0], arr[i][1], wait[i], tat[i]);
+    }
+    printf("The average wait time is: %lf and the average turn around time is: %lf\n", b, a);
     return 0;
 }
-void findWaitingTime(int *processes, int n, int *bt, int *wt) {
-    wt[0] = 0;
-    for (int i = 1; i < n; i++) {
-        wt[i] = bt[i - 1] + wt[i - 1];
-    }
-}
-void findTurnAroundTime(int *processes, int n, int *bt, int *wt, int *tat) {
+void sort(int a[][2], int n) {
     for (int i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
+        for (int j = i + 1; j < n; j++) {
+            if (a[i][0] > a[j][0]) {
+                int t = a[i][0];
+                int k = a[i][1];
+                a[i][0] = a[j][0];
+                a[i][1] = a[j][1];
+                a[j][0] = t;
+                a[j][1] = k;
+            }
+        }
     }
-}
-void findAverageTime(int *processes, int n, int *bt) {
-    int wt[n], tat[n], total_wt = 0, total_tat = 0;
-    findWaitingTime(processes, n, bt, wt);
-    findTurnAroundTime(processes, n, bt, wt, tat);
-    printf("Processes \t Burst time \t  Waiting time \t  Turn around time\n");
-    for (int i = 0; i < n; i++) {
-        total_wt = total_wt + wt[i];
-        total_tat = total_tat + tat[i];
-        printf("%d \t\t %d \t\t %d \t\t %d\n", i + 1, bt[i], wt[i], tat[i]);
-    }
-    float k = (float)total_wt / (float)n;
-    float l = (float)total_tat / (float)n;
-    printf("Average waiting time = %f\n", k);
-    printf("Average turn around time = %f\n", l);
 }
--- sjf.c
+++ sjf.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
-int main()
-{
+int main() {
     printf("Ayam Dobhal | Section C | University Roll Number 2016685\n");
-    int bt[20], p[20], wt[20], tat[20], i, j, n, total = 0, pos, temp;
-    float avg_wt, avg_tat;
-    printf("Enter number of processes: ");
+    int n;
+    printf("Enter the no. of processes: ");
     scanf("%d", &n);
-    printf("Enter Burst Time: ");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &bt[i]);
-        p[i] = i + 1;
+    int arr[n][2], burst[n];
+    for (int i = 0; i < n; i++) {
+        printf("Enter the arrival and burst time for the %dth process: ", i + 1);
+        scanf("%d%d", &arr[i][0], &arr[i][1]);
+        burst[i] = arr[i][1];
     }
-    for (i = 0; i < n; i++) {
-        pos = i;
-        for (j = i + 1; j < n; j++) {
-            if (bt[j] < bt[pos])
-                pos = j;
+    int wait[n], tat[n], prev[n];
+    int t = 0, averageTAT = 0, averageWait = 0;
+    for (int i = 0; i < n; i++) {
+        wait[i] = 0;
+        prev[i] = 0;
+    }
+    while (1) {
+        int ind = 0, mini = 100000;
+        for (int i = 0; i < n; i++) {
+            if (t >= arr[i][0] && mini > arr[i][1] && arr[i][1] > 0)
+            {
+                mini = arr[i][1];
+                ind = i;
+            }
         }
-        temp = bt[i];
-        bt[i] = bt[pos];
-        bt[pos] = temp;
-        temp = p[i];
-        p[i] = p[pos];
-        p[pos] = temp;
+        if (mini == 100000)
+            break;
+        arr[ind][1] -= 1;
+        wait[ind] += t - prev[ind];
+        t++;
+        prev[ind] = t;
     }
-    wt[0] = 0;
-    for (i = 1; i < n; i++) {
-        wt[i] = 0;
-        for (j = 0; j < i; j++)
-            wt[i] += bt[j];
-        total += wt[i];
+    for (int i = 0; i < n; i++) {
+        tat[i] = wait[i] + burst[i];
+        averageTAT += tat[i];
+        averageWait += wait[i];
     }
-    avg_wt = (float)total / n;
-    total = 0;
-    printf("\n Process  \t Burst Time \t Waiting Time \t\t Turnaround Time\n");
-    for (i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
-        total += tat[i];
-        printf("p%d \t\t\t  %d \t\t\t    %d\t\t\t\t%d\n", p[i], bt[i], wt[i], tat[i]);
+    double a = averageTAT / (1.0 * n), b = averageWait / (1.0 * n);
+    printf("Process no.\t Arrival time \t Burst Time\t Waiting Time \t Turn around Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t %d\t\t %d \t\t %d \t\t%d\n", i + 1, arr[i][0], arr[i][1], wait[i], tat[i]);
     }
-    avg_tat = (float)total / n;
-    printf("Average Waiting Time=%f\n", avg_wt);
-    printf("Average Turnaround Time=%f\n", avg_tat);
+    printf("The average wait time is: %lf and the average turn around time is: %lf\n", b, a);
+    return 0;
 }
